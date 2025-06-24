bf9c399406ff9a903b86146d02de38d2d926aea7
maliakaljenson <126373230+maliakaljenson@users.noreply.github.com>
2025-06-24 12:50:39 +0530

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,7 +1,7 @@
 # Simple Python Program
 
 # Print a message
-print("yyyyyyTTTTTYYYYYGGG, World!!!!!!!!####  ***")
+print("yyyyyyTTTTTYYYYYGGGEEE, World!!!!!!!!####  ***")
 
 # Add two numbers
 a = 10
