da81d90f24f06a27a06e155e4c8732d1a4134f14
maliakaljenson <126373230+maliakaljenson@users.noreply.github.com>
2025-07-04 12:59:46 +0530

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,12 +1,12 @@
 # Simple Python Program
 
 # Print a message
-print("yyyyyyTTTTTYYYYYGGGEEE, World!!!!!!!!####  ***")
+
 
 # Add two numbers
 a = 10
 b = 5
-sum = a ++ b
+sum = a + b
 
 # Display the result
 print("Sum of a&B ", a, "and", b, "is", sum)
